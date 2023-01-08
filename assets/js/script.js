d06ac80f3e314c36b830ae4fa862daadbda08c1c
Neochees <113420296+Neochees@users.noreply.github.com>
2023-01-08 16:45:36 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("csharp").onclick = function(){
+	var csharp_toets = document.getElementById("audio_csharp");
+	csharp_toets.play();
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("dsharp").onclick = function(){
+	var dsharp_toets = document.getElementById("audio_dsharp");
+	dsharp_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
+document.getElementById("fsharp").onclick = function(){
+	var fsharp_toets = document.getElementById("audio_fsharp");
+	fsharp_toets.play();
+}
+document.getElementById("g").onclick = function(){
+	var g_toets = document.getElementById("audio_g");
+	g_toets.play();
+}
+document.getElementById("gsharp").onclick = function(){
+	var gsharp_toets = document.getElementById("audio_gsharp");
+	gsharp_toets.play();
+}
+document.getElementById("a").onclick = function(){
+	var a_toets = document.getElementById("audio_a");
+	a_toets.play();
+}
+document.getElementById("asharp").onclick = function(){
+	var asharp_toets = document.getElementById("audio_asharp");
+	asharp_toets.play();
+}
+document.getElementById("b").onclick = function(){
+	var b_toets = document.getElementById("audio_b");
+	b_toets.play();
+}
+document.getElementById("ctwo").onclick = function(){
+	var ctwo_toets = document.getElementById("audio_ctwo");
+	ctwo_toets.play();
+}
+document.getElementById("csharptwo").onclick = function(){
+	var csharptwo_toets = document.getElementById("audio_csharptwo");
+	csharptwo_toets.play();
+}
+document.getElementById("dtwo").onclick = function(){
+	var dtwo_toets = document.getElementById("audio_dtwo");
+	dtwo_toets.play();
+}
